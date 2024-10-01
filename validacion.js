347552f6aeb6bb7f58d46459f257d6678b35bea5
KJC-dev11 <kevinjofre1111@gmail.com>
2024-10-01 01:02:12 -0300

    123

--- validacion.js
+++ validacion.js
@@ -1,12 +1,12 @@
-function validar() {
-    // Extraer los inputs
-    let user = document.forms['form-contacto']['user'];
+function validar(event) {
+    event.preventDefault();
+
+    let user = document.forms['form-contacto']['nombre']; 
     let mail = document.forms['form-contacto']['mail'];
-    let celular = document.getElementById('celular');  // opcional, ya que no es requerido
+    let celular = document.getElementById('celular'); 
 
     let isValid = true;
 
-    // Validar el primer input (Nombre)
     if (user.value === "" || user.value.length < 4 || user.value.length > 10) {
         document.getElementById('spanErrorUser').style.display = 'block';
         isValid = false;
@@ -14,7 +14,6 @@ function validar() {
         document.getElementById('spanErrorUser').style.display = 'none';
     }
 
-    // Validar el segundo input (Correo Electrónico)
     if (mail.value === "") {
         document.getElementById('spanErrorMail').style.display = 'block';
         isValid = false;
@@ -22,7 +21,14 @@ function validar() {
         document.getElementById('spanErrorMail').style.display = 'none';
     }
 
-    // Si todo es válido, procesar el envío y mostrar el mensaje
+    const telefonoPattern = /^[0-9]{10}$/;
+    if (celular.value === "" || !telefonoPattern.test(celular.value)) {
+        document.getElementById('spanErrorCelular').style.display = 'block';
+        isValid = false;
+    } else {
+        document.getElementById('spanErrorCelular').style.display = 'none';
+    }
+
     if (isValid) {
         let res = document.getElementById('resultado');
         let text = document.createElement('h2');
@@ -34,8 +40,8 @@ function validar() {
         // Limpiar los campos del formulario
         user.value = '';
         mail.value = '';
-        celular.value = '';  // Esto es opcional, ya que el campo de teléfono no es requerido
+        celular.value = '';
     }
 
-    return false; // Prevenir la actualización de la página
+    return false; // Asegurar que el formulario no se envíe
 }
